4e56c14dbb541dfaea641650357edf4c9846b03e
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-07-19 02:33:11 +0530

    Add buttons for demo purposes

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,33 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const [emptyMessage, setEmptyMessage] = useState(false);
+  const [wrongURLMessage, setWrongURLMessage] = useState(false);
+
   async function fetchMoviesHandler() {
     // clear errors, render loading message
     setIsLoading(true);
     setError(false);
 
     try {
-      const response = await fetch("https://swapi.dev/api/film"); // deliberate error, correct => '/films'
+      const response = await fetch(
+        "https://swapi.dev/api/film" + (wrongURLMessage ? "" : "s")
+      ); // correct end point is /films
 
       if (!response.ok) throw new Error("HTTP error");
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
+      const transformedMovies = (emptyMessage ? [] : data.results).map(
+        (movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        }
+      );
       setMovies(transformedMovies);
     } catch (error) {
       setError(error);
@@ -36,21 +43,30 @@ function App() {
     }
   }
 
+  let content = <p>No movies found</p>;
+  if (!isLoading && !error) {
+    if (movies.length) content = <MoviesList movies={movies} />;
+  } else content = null;
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
+        Using {!emptyMessage && "non-"}empty movie list &nbsp;
+        <button onClick={() => setEmptyMessage((prev) => !prev)}>Toggle</button>
+        <br />
+        <br />
+        Using {wrongURLMessage ? "wrong" : "correct"} URL &nbsp;
+        <button onClick={() => setWrongURLMessage((prev) => !prev)}>
+          Toggle
+        </button>
+      </section>
+      <section>
         {isLoading && "Loading..."}
         {error && error.toString()}
-        {!isLoading &&
-          !error &&
-          (movies.length !== 0 ? (
-            <MoviesList movies={movies} />
-          ) : (
-            <p>No movies found</p>
-          ))}
+        {content}
       </section>
     </React.Fragment>
   );
